780541c01e95ef74cac66351d64d78482de09f1e
Tapas1380 <tapasranjanhr@gmail.com>
2025-06-30 14:28:15 +0530

    msg

--- src/main/java/com/example/authdemo/LoginAppApplication.java
+++ src/main/java/com/example/authdemo/LoginAppApplication.java
@@ -2,11 +2,13 @@ package com.example.authdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class LoginAppApplication {
 
 	public static void main(String[] args) {
+	
 		SpringApplication.run(LoginAppApplication.class, args);
 	}
 
--- src/test/java/com/mvc/LoginAppApplicationTests.java
+++ src/test/java/com/mvc/LoginAppApplicationTests.java
@@ -2,9 +2,12 @@ package com.mvc;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+
 import com.example.authdemo.LoginAppApplication;
 
 @SpringBootTest(classes = LoginAppApplication.class)
+
 class LoginAppApplicationTests {
 
 	@Test
